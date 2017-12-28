d56bc86b9641dc72e8e65d0f14e271be5d158c59
red <seokz20@gmail.com>
2017-12-28 17:34:25 +0900

    FetchedResultsController 를 통해 CRUD 하도록 변경.

--- Notes/View Controllers/NotesViewController.swift
+++ Notes/View Controllers/NotesViewController.swift
@@ -16,14 +16,18 @@ struct Segue {
 
 final class NotesViewController: UIViewController {
     
-    private var hasNotes: Bool {
-        return notes?.count ?? 0 > 0
-    }
+    private lazy var fetchedResultsController: NSFetchedResultsController<Note> = {
+        let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: #keyPath(Note.updatedAt), ascending: false)]
+        
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: coreDataManager.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        fetchedResultsController.delegate = self
+        return fetchedResultsController
+    }()
     
-    private var notes: [Note]? {
-        didSet {
-            updateView()
-        }
+    private var hasNotes: Bool {
+        return fetchedResultsController.fetchedObjects?.count ?? 0 > 0
     }
     
     private lazy var updatedAtDateFormatter: DateFormatter = {
@@ -44,7 +48,8 @@ final class NotesViewController: UIViewController {
         title = "Notes"
         
         fetchNotes()
-        setupNotificationHandling() 
+
+        updateView()
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -59,10 +64,10 @@ final class NotesViewController: UIViewController {
             destination.managedObjectContext = coreDataManager.managedObjectContext
         case Segue.note:
             guard let destination = segue.destination as? NoteViewController else { return }
-            guard let indexPath = tableView.indexPathForSelectedRow, let note = notes?[indexPath.row] else {
+            guard let indexPath = tableView.indexPathForSelectedRow else {
                 return
             }
-
+            let note = fetchedResultsController.object(at: indexPath)
             destination.note = note
         default:
             break
@@ -71,91 +76,47 @@ final class NotesViewController: UIViewController {
     }
     
     private func fetchNotes() {
-        let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: #keyPath(Note.updatedAt), ascending: false)]
         
-        coreDataManager.managedObjectContext.performAndWait({
-            do {
-                let notes = try fetchRequest.execute()
-                self.notes = notes
-                self.tableView.reloadData()
-            } catch {
-                print("Unable to Execute Fetch Request")
-                print("\(error), \(error.localizedDescription)")
-            }
-        })
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print("Unable to Perform Fetch Request")
+            print("\(error), \(error.localizedDescription)")
+        }
+        
     }
     
     private func updateView() {
         tableView.isHidden = !hasNotes
         messageLabel.isHidden = hasNotes
     }
-    
-    private func setupNotificationHandling() {
-        let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(self, selector: #selector(managedObjectContextObjectsDidChange(_:)), name: NSNotification.Name.NSManagedObjectContextObjectsDidChange, object: coreDataManager.managedObjectContext)
-    }
-    
-    
-    @objc private func managedObjectContextObjectsDidChange(_ notification: Notification) {
-        var notesDidChange = false
-        guard let userInfo = notification.userInfo else {
-            return
-        }
-        if let inserts = userInfo[NSInsertedObjectsKey] as? Set<Note> {
-            for note in inserts {
-                notes?.append(note)
-                notesDidChange = true
-            }
-        }
+
+    fileprivate func configure(_ cell: NoteTableViewCell, at indexPath: IndexPath) {
+        let note = fetchedResultsController.object(at: indexPath)
         
-        if let updates = userInfo[NSUpdatedObjectsKey] as? Set<Note> {
-            for _ in updates {
-                notesDidChange = true
-            }
-        }
+        cell.titleLabel.text = note.title
+        cell.contentsLabel.text = note.contents
+        cell.updatedAtLabel.text = updatedAtDateFormatter.string(from: note.updatedAtAsDate)
         
-        if let deletes = userInfo[NSDeletedObjectsKey] as? Set<Note> {
-            for note in deletes {
-                if let index = notes?.index(of: note) {
-                    notes?.remove(at: index)
-                    notesDidChange = true
-                }
-            }
-        }
-        
-        if notesDidChange {
-            notes?.sort(by: { $0.updatedAtAsDate > $1.updatedAtAsDate })
-            
-            tableView.reloadData()
-            
-            updateView()
-        }
     }
 }
 
 extension NotesViewController: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return hasNotes ? 1 : 0
+        return fetchedResultsController.sections?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notes?.count ?? 0
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let note = notes?[indexPath.row] else {
-            fatalError("Unexpected IndexPath")
-        }
-        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: NoteTableViewCell.reuseIdentifier, for: indexPath) as? NoteTableViewCell else {
             fatalError("Unexpected IndexPath")
         }
         
-        cell.titleLabel.text = note.title
-        cell.contentsLabel.text = note.contents
-        cell.updatedAtLabel.text = updatedAtDateFormatter.string(from: note.updatedAtAsDate)
+        configure(cell, at: indexPath)
         
         return cell
     }
@@ -163,11 +124,45 @@ extension NotesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
         
-        guard let note = notes?[indexPath.row] else {
-            fatalError("Unexpected IndexPath")
-        }
+        let note = fetchedResultsController.object(at: indexPath)
         
         note.managedObjectContext?.delete(note)
     }
     
+}
+
+extension NotesViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+        updateView()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        case .update:
+            if let indexPath = indexPath, let cell = tableView.cellForRow(at: indexPath) as? NoteTableViewCell {
+                configure(cell, at: indexPath)
+            }
+        case .move:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+        }
+    }
 }
